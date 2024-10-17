2ff94486731ac71e35552446650ea505fd71cb88
Nikos Gianniotis <ngiann@gmail.com>
2024-10-17 14:06:09 +0200

    maximise sum of two elbos

--- ext/ELBOfyExt.jl
+++ ext/ELBOfyExt.jl
@@ -2,7 +2,9 @@ module ELBOfyExt # Should be same name as the file (just like a normal package)
 
     using ELBOfyUtilities, ELBOfy, Optim
 
+    #-------------------------------------------------------------------------------------------------------------------------------------
     function ELBOfyUtilities.maximise_elbo(elbo::T, params; iterations = 1000, show_trace = true, g_tol=1e-4) where T<:ELBOfy.AbstractElbo
+    #-------------------------------------------------------------------------------------------------------------------------------------
 
         opt = Optim.Options(show_trace = show_trace, iterations = iterations, allow_f_increases = false, g_tol = g_tol)
 
@@ -18,14 +20,38 @@ module ELBOfyExt # Should be same name as the file (just like a normal package)
 
     end
 
-    function ELBOfyUtilities.maximise_elbo(elbo::T; iterations = 1000, show_trace = true, g_tol = 1e-4) where T<:ELBOfy.AbstractElbo
 
-        ELBOfyUtilities.maximise_elbo(elbo, randn(numparam(elbo)); iterations = iterations, show_trace = show_trace, g_tol = g_tol)
+    # #-------------------------------------------------------------------------------------------------------------------------------------
+    # function ELBOfyUtilities.maximise_elbo(elbo::T; iterations = 1000, show_trace = true, g_tol = 1e-4) where T<:ELBOfy.AbstractElbo
+    # #-------------------------------------------------------------------------------------------------------------------------------------
+
+    #     ELBOfyUtilities.maximise_elbo(elbo, randn(numparam(elbo)); iterations = iterations, show_trace = show_trace, g_tol = g_tol)
+
+    # end
+
+
+    #-------------------------------------------------------------------------------------------------------------------------------------
+    function ELBOfyUtilities.maximise_elbo(elbos::Vector{T}, params; iterations = 1000, show_trace = true, g_tol=1e-4) where T<:ELBOfy.AbstractElbo
+    #-------------------------------------------------------------------------------------------------------------------------------------
+
+        opt = Optim.Options(show_trace = show_trace, iterations = iterations, allow_f_increases = false, g_tol = g_tol)
+
+        # if has_logp_gradient(elbo)
+
+        #     gradhelper!(st, param) = copyto!(st, -ELBOfy.grad(elbo, param))
+        
+        #     return optimize(x-> -elbo(x), params, ConjugateGradient(), opt)
+
+        # end
+
+        return optimize(x-> -elbos(x), params, NelderMead(), opt)
 
     end
 
 
+    #-------------------------------------------------------------------------------------------------------------------------------------
     function ELBOfyUtilities.updatecovariance(elbo::ELBOfy.ElboMVIExt, param; minimumeigenvalue = 1e-6)
+    #-------------------------------------------------------------------------------------------------------------------------------------
         
         μ, Cprv = ELBOfy.getμC(elbo, param)
     
