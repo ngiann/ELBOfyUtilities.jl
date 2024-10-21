ed8eb3a7eeac6eec1039d6fc28ec6dc0a2c069e9
Nikos Gianniotis <ngiann@gmail.com>
2024-10-21 21:33:59 +0200

    account parameters in ψ ; remove commented out code

--- ext/ELBOfyExt.jl
+++ ext/ELBOfyExt.jl
@@ -6,12 +6,16 @@ module ELBOfyExt # Should be same name as the file (just like a normal package)
     function ELBOfyUtilities.maximise_elbo(elbo::T, params; iterations = 1000, show_trace = true, g_tol=1e-4) where T<:ELBOfy.AbstractElbo
     #-------------------------------------------------------------------------------------------------------------------------------------
 
-        opt = Optim.Options(show_trace = show_trace, iterations = iterations, allow_f_increases = false, g_tol = g_tol)
+        opt = Optim.Options(show_trace = show_trace, show_every=2, iterations = iterations, allow_f_increases = false, g_tol = g_tol)
 
         if has_logp_gradient(elbo)
 
+            # display("gradient opt")
+
             gradhelper!(st, param) = copyto!(st, -ELBOfy.grad(elbo, param))
         
+            # params = optimize(x-> -elbo(x), params, ParticleSwarm(), Optim.Options(iterations=1000, show_every=100, show_trace=true)).minimizer
+
             return optimize(x-> -elbo(x), params, ConjugateGradient(), opt)
 
         end
@@ -21,47 +25,21 @@ module ELBOfyExt # Should be same name as the file (just like a normal package)
     end
 
 
-    # #-------------------------------------------------------------------------------------------------------------------------------------
-    # function ELBOfyUtilities.maximise_elbo(elbo::T; iterations = 1000, show_trace = true, g_tol = 1e-4) where T<:ELBOfy.AbstractElbo
-    # #-------------------------------------------------------------------------------------------------------------------------------------
-
-    #     ELBOfyUtilities.maximise_elbo(elbo, randn(numparam(elbo)); iterations = iterations, show_trace = show_trace, g_tol = g_tol)
-
-    # end
-
-
-    # #-------------------------------------------------------------------------------------------------------------------------------------
-    # function ELBOfyUtilities.maximise_elbo(elbos::Vector{T}, params; iterations = 1000, show_trace = true, g_tol=1e-4) where T<:ELBOfy.AbstractElbo
-    # #-------------------------------------------------------------------------------------------------------------------------------------
-
-    #     opt = Optim.Options(show_trace = show_trace, iterations = iterations, allow_f_increases = false, g_tol = g_tol)
-
-    #     # if has_logp_gradient(elbo)
-
-    #     #     gradhelper!(st, param) = copyto!(st, -ELBOfy.grad(elbo, param))
-        
-    #     #     return optimize(x-> -elbo(x), params, ConjugateGradient(), opt)
-
-    #     # end
-
-    #     return optimize(x-> -elbos(x), params, NelderMead(), opt)
-
-    # end
-
-
     #-------------------------------------------------------------------------------------------------------------------------------------
     function ELBOfyUtilities.updatecovariance(elbo::ELBOfy.ElboMVIExt, param; minimumeigenvalue = 1e-6)
     #-------------------------------------------------------------------------------------------------------------------------------------
-        
-        μ, Cprv = ELBOfy.getμC(elbo, param)
+  
+        μ, Cprv, ψ = ELBOfy.getμCψ(elbo, param)
     
         Σnew = getcovariance(elbo.logp, μ; minimumeigenvalue = minimumeigenvalue)
     
         Vnew, = ELBOfy.eigendecomposition(Σnew)
     
-        elbonew = ELBOfy.ElboMVIExt(elbo.Z, elbo.D, elbo.S, elbo.logp, elbo.gradlogp, elbo.howtorun, Vnew, Cprv)
+        elbonew = ELBOfy.ElboMVIExt(elbo.Z, elbo.D, elbo.d, elbo.S, elbo.logp, elbo.gradlogp, elbo.howtorun, Vnew, Cprv)
     
-        elbonew, [μ; zeros(elbo.D); 1.0]
+        elbonew, [μ; zeros(elbo.D); 1.0; ψ] # set mean μ to current mean
+                                            # set eigenvalues to zero, this makes the contribution of the new covariance zero
+                                            # set t to 1, this retains the previous solution
     end
 
 end # module
