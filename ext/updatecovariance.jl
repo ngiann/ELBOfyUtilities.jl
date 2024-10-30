ec7430130e854be5531fd9b6f5798a847c28a6fc
Nikos Gianniotis <ngiann@gmail.com>
2024-10-30 21:57:14 +0100

    specialise signatures

--- ext/updatecovariance.jl
+++ ext/updatecovariance.jl
@@ -1,5 +1,5 @@
 #-------------------------------------------------------------------------------------------------------------------------------------
-function ELBOfyUtilities.updatecovariance(elbo::ELBOfy.ElboMVIExt, param)
+function ELBOfyUtilities.updatecovariance(elbo::ELBOfy.ElboMVIExt, param::Vector)
 #-------------------------------------------------------------------------------------------------------------------------------------
 
     μ, Cprv, ψ = ELBOfy.getμCψ(elbo, param)
@@ -18,6 +18,23 @@ function ELBOfyUtilities.updatecovariance(elbo::ELBOfy.ElboMVIExt, param)
 end
 
 
-ELBOfyUtilities.updatecovariance(elbo::ELBOfy.ElboMVIExt, res::Optim.OptimizationResults) = ELBOfyUtilities.updatecovariance(elbo, getsolution(res))
+ELBOfyUtilities.updatecovariance(elbo, res::Optim.OptimizationResults) = ELBOfyUtilities.updatecovariance(elbo, getsolution(res))
 
-ELBOfyUtilities.updatecovariance(elbo::ELBOfy.ElboMVIExt, res::BlackBoxOptim.OptimizationResults) = ELBOfyUtilities.updatecovariance(elbo, getsolution(res))
+ELBOfyUtilities.updatecovariance(elbo, res::BlackBoxOptim.OptimizationResults) = ELBOfyUtilities.updatecovariance(elbo, getsolution(res))
+
+
+#-------------------------------------------------------------------------------------------------------------------------------------
+function ELBOfyUtilities.updatecovariance(elbo::ELBOfy.ElboSkewDiagExt, param::Vector)
+#-------------------------------------------------------------------------------------------------------------------------------------
+
+    μ, Cprv, δ, ψ = ELBOfy.getμCδψ(elbo, param)
+
+    Vnew = geteigenvectors(elbo.logp, μ)
+
+    elbonew = ELBOfy.ElboSkewDiagExt(elbo.Z, elbo.D, elbo.d, elbo.S, elbo.logp, elbo.gradlogp, elbo.parallel, Vnew, Cprv)
+
+    elbonew, [μ; zeros(elbo.D); δ; 1.0; ψ] # set mean μ to current mean
+                                           # set eigenvalues to zero, this makes the contribution of the new covariance zero
+                                           # set t to 1, this retains the previous solution
+end
+    
