2fd58b19c713be0fd9c9fae7eb181558516fa95d
Nikos Gianniotis <ngiann@gmail.com>
2024-11-02 15:36:21 +0100

    add new deps

--- ext/ELBOfyExt.jl
+++ ext/ELBOfyExt.jl
@@ -1,9 +1,13 @@
 module ELBOfyExt # Should be same name as the file (just like a normal package)
 
-    using ELBOfyUtilities, ELBOfy, Optim, Printf
+    using ELBOfyUtilities, ELBOfy, Optim, Printf, Random
     using BlackBoxOptim
+    using Evolutionary
 
     ELBOfyUtilities.getsolution(res::Vector) = res
+  
+    ELBOfyUtilities.getminimum(res::Evolutionary.OptimizationResults) = Evolutionary.minimum(res)
+    ELBOfyUtilities.getsolution(res::Evolutionary.OptimizationResults) = Evolutionary.minimizer(res)
   
     ELBOfyUtilities.getminimum(res::BlackBoxOptim.OptimizationResults) = BlackBoxOptim.best_fitness(res)
     ELBOfyUtilities.getsolution(res::BlackBoxOptim.OptimizationResults) = BlackBoxOptim.best_candidate(res)
@@ -16,6 +20,8 @@ module ELBOfyExt # Should be same name as the file (just like a normal package)
     include("maximise_elbo.jl")
 
     include("maximise_elbo_blackboxoptim.jl")
+
+    include("maximise_elbo_cmaes.jl")
 
     include("get_callback_and_track_elbo_for_tracking_test_evidence.jl")
 
