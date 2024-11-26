0b84a57f46eaed92870c3aa345ea59d75fa826f3
Nikos Gianniotis <ngiann@gmail.com>
2024-11-26 09:28:20 +0100

    using DifferentiationInterface.jl

--- ext/maximise_elbo.jl
+++ ext/maximise_elbo.jl
@@ -1,25 +1,25 @@
 #-------------------------------------------------------------------------------------------------------------------------------------
-function ELBOfyUtilities.maximise_elbo(elbo::T, res::Optim.OptimizationResults; iterations = 1000, iteration_test = 0, show_every = 1, g_tol=1e-8, Method=BFGS()) where T<:ELBOfy.AbstractElbo
+function ELBOfyUtilities.maximise_elbo(elbo::T, res::Optim.OptimizationResults; iterations = 1000, iteration_test = 0, show_every = 1, g_tol=1e-8, Method=BFGS(), backend = AutoZygote()) where T<:ELBOfy.AbstractElbo
 #-------------------------------------------------------------------------------------------------------------------------------------
 
     params = res.minimizer
 
-    ELBOfyUtilities.maximise_elbo(elbo, params; iterations = iterations, iteration_test = iteration_test, show_every = show_every, g_tol = g_tol, Method = Method)
+    ELBOfyUtilities.maximise_elbo(elbo, params; iterations = iterations, iteration_test = iteration_test, show_every = show_every, g_tol = g_tol, Method = Method, backend = backend)
 
 end
 
 
 #-------------------------------------------------------------------------------------------------------------------------------------
-function ELBOfyUtilities.maximise_elbo(elbo::T; iterations = 1000, iteration_test = 0, show_every = 1, g_tol=1e-8, Method = BFGS()) where T<:ELBOfy.AbstractElbo
+function ELBOfyUtilities.maximise_elbo(elbo::T; iterations = 1000, iteration_test = 0, show_every = 1, g_tol=1e-8, Method = BFGS(), backend = AutoZygote()) where T<:ELBOfy.AbstractElbo
 #-------------------------------------------------------------------------------------------------------------------------------------
 
-    ELBOfyUtilities.maximise_elbo(elbo, randn(ELBOfy.numparam(elbo)); iterations = iterations, iteration_test = iteration_test, show_every = show_every, g_tol = g_tol, Method = Method)
+    ELBOfyUtilities.maximise_elbo(elbo, randn(ELBOfy.numparam(elbo)); iterations = iterations, iteration_test = iteration_test, show_every = show_every, g_tol = g_tol, Method = Method, backend = backend)
 
 end
 
 
 #-------------------------------------------------------------------------------------------------------------------------------------
-function ELBOfyUtilities.maximise_elbo(elbo::T, params; iterations = 1000, iteration_test = 0, show_every = 1, g_tol=1e-8, Method = BFGS()) where T<:ELBOfy.AbstractElbo
+function ELBOfyUtilities.maximise_elbo(elbo::T, params; iterations = 1000, iteration_test = 0, show_every = 1, g_tol=1e-8, Method = BFGS(), backend = AutoZygote()) where T<:ELBOfy.AbstractElbo
 #-------------------------------------------------------------------------------------------------------------------------------------
 
     @printf("Resuming optimisation starting with approximate elbo of %f\n", elbo(params))
@@ -28,7 +28,7 @@ function ELBOfyUtilities.maximise_elbo(elbo::T, params; iterations = 1000, itera
 
     opt = Optim.Options(show_trace = show_every > 0, show_every = max(show_every,1), iterations = iterations, allow_f_increases = false, g_tol = g_tol)
 
-    g!(storage, params) = copyto!(storage, -1*Zygote.gradient(elbo, params)[1])
+    g!(storage, params) = copyto!(storage, -1*DifferentiationInterface.gradient(elbo, backend, params))
 
     # if has_logp_gradient(elbo)
 
