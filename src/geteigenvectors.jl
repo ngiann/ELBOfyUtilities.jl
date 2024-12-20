e24c39a9e1b71842d3724c93a3eaf25f3ceff23e
Nikos Gianniotis <ngiann@gmail.com>
2024-12-20 11:54:32 +0100

    simplify

--- src/geteigenvectors.jl
+++ src/geteigenvectors.jl
@@ -1,12 +1,7 @@
 function geteigenvectors(logposterior, mode)
 
-    # H = -ForwardDiff.hessian(logposterior, mode) # neative hessian
-    
-    H = -FiniteDiff.finite_difference_hessian(logposterior, mode)
+    H = FiniteDiff.finite_difference_hessian(logposterior, mode)
 
-    # don't invert H , just get eigenvectors.
-    # Pay attention to sorting!!!!
-
-    eigen(H, sortby=-).vectors
+    eigen(H).vectors
 
 end
